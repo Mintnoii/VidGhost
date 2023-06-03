8cd749c7455c4425c331f33c591d33102ecde104
Mintnoii <MintnoiiQing@gmail.com>
2023-06-03 23:30:21 +0800

    demo.mp4

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-
+  <div class="container">
+    <div class="materials">
+      素材区
+    </div>
+    <div>
+      <div>
+      预览区
+    </div>
+    <div>
+      操作区
+    </div>
+    </div>
   </div>
 </template>
 
@@ -9,5 +19,16 @@
 </script>
 
 <style scoped>
-
+.container {
+  display: flex;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  text-align: center;
+}
+.materials {
+  height: 100vh;
+  width: 20vw; 
+}
 </style>
